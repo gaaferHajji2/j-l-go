ec4436553779847935cc83d2ff2661071813e41c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-22 13:41:51 +0300

    Write simple todolist items

--- From-Youtube-01/main.go
+++ From-Youtube-01/main.go
@@ -4,5 +4,13 @@ import "fmt"
 
 func main() {
 	// fmt.Print("Hello Go World!\n")
-	fmt.Print("Welcome to our Todolist App!\n")
+	fmt.Println("##### Welcome to our Todolist App! #####")
+
+	fmt.Println("1. Watch the crash course of Go")
+
+	fmt.Println("2. Build network automation tools with Go")
+
+	fmt.Println("3. Build Microservices Applications With gRPC")
+
+	fmt.Println("4. Build my Applications using Go")
 }
