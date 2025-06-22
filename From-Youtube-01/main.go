4cd29be563869eda29c3f76be24797b66b859d81
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-22 13:48:41 +0300

    Organize my todos In groups

--- From-Youtube-01/main.go
+++ From-Youtube-01/main.go
@@ -6,6 +6,8 @@ func main() {
 	// fmt.Print("Hello Go World!\n")
 	fmt.Println("##### Welcome to our Todolist App! #####")
 
+	fmt.Println("*** List of my todos ***")
+
 	fmt.Println("1. Watch the crash course of Go")
 
 	fmt.Println("2. Build network automation tools with Go")
@@ -13,4 +15,10 @@ func main() {
 	fmt.Println("3. Build Microservices Applications With gRPC")
 
 	fmt.Println("4. Build my Applications using Go")
+
+	fmt.Println("*** List of my Tutorials ***")
+
+	fmt.Println("2. Build network automation tools with Go")
+
+	fmt.Println("3. Build Microservices Applications With gRPC")
 }
