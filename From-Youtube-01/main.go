e90527efc9485309c1d51b3c1c134b18c76b6338
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-22 14:13:42 +0300

    Refactor code using variables

--- From-Youtube-01/main.go
+++ From-Youtube-01/main.go
@@ -4,28 +4,37 @@ import "fmt"
 
 func main() {
 	// fmt.Print("Hello Go World!\n")
+
+	var task01 = "1. Watch the crash course of Go"
+
+	var task02 = "2. Build network automation tools with Go"
+
+	var task03 = "3. Build Microservices Applications With gRPC"
+
+	var task04 = "4. Build my Applications using Go"
+
 	fmt.Println("##### Welcome to our Todolist App! #####")
 
 	fmt.Println("*** List of my todos ***")
 
-	fmt.Println("1. Watch the crash course of Go")
+	fmt.Println(task01)
 
-	fmt.Println("2. Build network automation tools with Go")
+	fmt.Println(task02)
 
-	fmt.Println("3. Build Microservices Applications With gRPC")
+	fmt.Println(task03)
 
-	fmt.Println("4. Build my Applications using Go")
+	fmt.Println(task04)
 
 	fmt.Println()
 
 	fmt.Println("*** List of my Tutorials ***")
 
-	fmt.Println("2. Build network automation tools with Go")
+	fmt.Println(task02)
 
-	fmt.Println("3. Build Microservices Applications With gRPC")
+	fmt.Println(task03)
 
 	fmt.Println()
 
 	fmt.Println("*** My Goals ***")
-	fmt.Println("4. Build my Applications using Go")
+	fmt.Println(task04)
 }
