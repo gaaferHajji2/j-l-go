d0d938fbfaede76acd8daa8c00ac88057712ef4f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-14 10:08:27 +0300

    Define function with params

--- From-Youtube-01/main.go
+++ From-Youtube-01/main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
-// Define global variables
-var task01 = "Watch the crash course of Go"
-
-var task02 = "Build network automation tools with Go"
-
-var task03 = "Build Microservices Applications With gRPC"
-
-var task04 = "Build my Applications using Go"
-
-var taskItems = []string{task01, task02, task03, task04}
-
 func main() {
-	printTasks()
+
+	var task01 = "Watch the crash course of Go"
+
+	var task02 = "Build network automation tools with Go"
+
+	var task03 = "Build Microservices Applications With gRPC"
+
+	var task04 = "Build my Applications using Go"
+
+	var taskItems = []string{task01, task02, task03, task04}
+
+	printTasks(taskItems)
 }
 
-func printTasks() {
+func printTasks(taskItems []string) {
 	fmt.Println("##### Welcome to our Todolist App! #####")
 
 	for index, task := range taskItems {
