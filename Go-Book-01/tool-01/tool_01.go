7afd8b683699bf15bc22faf324dc7ef456ec5f0e
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-05 15:29:02 +0300

    Build search executable tool for windows and linux

--- Go-Book-01/tool-01/tool_01.go
+++ Go-Book-01/tool-01/tool_01.go
@@ -4,7 +4,14 @@ import(
 	"fmt"
 	"os"
 	"path/filepath"
+	"os/exec"
 )
+
+func isExecutableViaLookPath(filename string) bool {
+	_, err := exec.LookPath(filename)
+	return err == nil
+}
+
 
 func main() {
 	args := os.Args
@@ -22,11 +29,15 @@ func main() {
 	for _, directory := range pathSplit {
 		fullpath := filepath.Join(directory, file)
 
+		// fmt.Println("The Fullpath is: ", fullpath)
+
 		// Check if the file exists in that directory (full path)
 		fileInfo, err := os.Stat(fullpath)
 
 		if err != nil {
 			continue
+		} else {
+			// fmt.Println("We don't have error: ")
 		}
 
 		mode := fileInfo.Mode()
@@ -34,12 +45,30 @@ func main() {
 		// Check if it's regular file (Not folder: useful for Linux-Systems)
 		if !mode.IsRegular() {
 			continue
+		} else {
+			// fmt.Println("Regular File")
 		}
 
-		// Check if the file is executable by os
+		// Check if the file is executable by os (For Linux Systems)
 		if mode&0111 != 0 {
+			fmt.Println("----------------------------")
 			fmt.Println("The full path is: ", fullpath)
-			break
+			fmt.Println("----------------------------")
+			return
+		} else {
+			// fmt.Println("Not executable")
+			// fmt.Println("The mode is: ", mode&0111)
 		}
+		
+		// For Windows Systems
+		if isExecutableViaLookPath(fullpath) {
+			fmt.Println("----------------------------")
+			fmt.Println("The full path is: ", fullpath)
+			fmt.Println("----------------------------")
+		}
+
+		// fmt.Println("++++++++++++++++++++++++++++++++")
 	}
+
+	fmt.Println("The end of search")
 }
