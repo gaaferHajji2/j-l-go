fe69675c79a70b8106dca1b804015a5b4a5a7641
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-06 11:04:24 +0300

    Remove unused white lines

--- Go-Book-01/logging-example-01/jloka-log-linux.go
+++ Go-Book-01/logging-example-01/jloka-log-linux.go
@@ -7,14 +7,11 @@ import (
 
 func main() {
 	jlokaLog, err := syslog.New(syslog.LOG_WARNING|syslog.LOG_DAEMON, "my-app")
-
 	if err != nil {
 		log.Println("The err is: ", err)
 		return
 	}
-
 	defer jlokaLog.Close()
-
 	log.SetOutput(jlokaLog)
 	log.Println("JLoka Logging-01")
 }
--- Go-Book-01/logging-example-01/jloka-log-windows.go
+++ Go-Book-01/logging-example-01/jloka-log-windows.go
@@ -8,11 +8,9 @@ import (
 
 func SetupSimpleWriter(logFile string) {
 	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	multiWriter := io.MultiWriter(os.Stdout, file)
 	log.SetOutput(multiWriter)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile )
