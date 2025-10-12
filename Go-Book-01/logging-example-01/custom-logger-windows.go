19afb14d6a9fc046fe1dea307eb1b1094574fb7a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-12 13:07:31 +0300

    Adding custom logger

---
+++ Go-Book-01/logging-example-01/custom-logger-windows.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
+func main() {
+	LOGFILE := path.Join(os.TempDir(), "JLoka-log.log")
+	fmt.Println("The Log-File is: ", LOGFILE)
+
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	jlokaLogger := log.New(f, "jloka", log.LstdFlags)
+	jlokaLogger.Println("Hi Jafar Loka Custom Logger World")
+	jlokaLogger.Println("I am ITE Developer")
+}
