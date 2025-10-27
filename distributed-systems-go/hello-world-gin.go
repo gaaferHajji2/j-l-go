629fbe3600428a48a71cf7c681c96bd446a946fa
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-27 13:50:42 +0300

    Refactor code

--- distributed-systems-go/hello-world-gin.go
+++ distributed-systems-go/hello-world-gin.go
@@ -2,15 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main() {
-	router := gin.Default();
-
-	router.GET("/", func (c *gin.Context) {
-		c.JSON(200, gin.H{
-			// the comma at the end is required
-			"msg": "Hello Jafar Loka World",
-		})
+func HelloWorldHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "Hello Jafar Loka World",
 	})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", HelloWorldHandler)
 
 	router.Run(":5000")
 }
