a264c30f93ce4ae483efbf9f86216147b1d41e4a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-28 11:13:05 +0300

    Build xml and path example

--- distributed-systems-go/hello-world-gin.go
+++ distributed-systems-go/hello-world-gin.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	XMLName xml.Name `xml:"PersonObject"`
+	// here the data and attr must start with capital letter
+	FullName string `xml:"FullName"`
+	Age      int16  `xml:"Age,attr"`
+}
 
 func HelloWorldHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -8,10 +19,25 @@ func HelloWorldHandler(c *gin.Context) {
 	})
 }
 
+func HelloWorldNameHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"msg": "Hello To JLoka World: " + name,
+	})
+}
+
+func FullNameHandler(c *gin.Context) {
+	c.XML(200, Person{FullName: "Jafar Loka-01", Age: 25})
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/", HelloWorldHandler)
+
+	router.GET("/:name", HelloWorldNameHandler)
+
+	router.GET("/fullName", FullNameHandler)
 
 	router.Run(":5000")
 }
