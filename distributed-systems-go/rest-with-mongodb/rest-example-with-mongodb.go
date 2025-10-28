896181b9b94a82b0141209af505848c58ed3daf5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-28 11:48:31 +0300

    Add xid and implement CreateMeal

--- distributed-systems-go/rest-with-mongodb/rest-example-with-mongodb.go
+++ distributed-systems-go/rest-with-mongodb/rest-example-with-mongodb.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Meal struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,8 +17,28 @@ type Meal struct {
 	CreatedAt    time.Time `json:"createdAt"`
 }
 
+var meals []Meal
+
+func CreateMealHandler(c *gin.Context) {
+	var meal Meal
+
+	if err := c.ShouldBindJSON(&meal); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": err.Error(),
+		})
+	}
+	meal.ID = xid.New().String()
+	meal.CreatedAt = time.Now()
+	meals = append(meals, meal)
+	c.JSON(http.StatusCreated, meal)
+}
+
 func main() {
 	router := gin.Default()
+
+	meals = make([]Meal, 0)
+
+	router.POST("/meals", CreateMealHandler)
 
 	router.Run()
 }
