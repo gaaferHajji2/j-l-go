0f55504a9dbdf51270c939ee3ebc4f4533d5e51b
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-28 11:31:58 +0300

    Build data model of meal

--- distributed-systems-go/rest-with-mongodb/rest-example-with-mongodb.go
+++ distributed-systems-go/rest-with-mongodb/rest-example-with-mongodb.go
@@ -1,6 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Meal struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	CreatedAt    time.Time `json:"createdAt"`
+}
 
 func main() {
 	router := gin.Default()
