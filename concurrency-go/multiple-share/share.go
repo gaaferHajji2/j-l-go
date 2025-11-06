d36fc6cad6da53f239bf7beba022c06b96786b41
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-06 14:55:34 +0300

    Solve the problem of multiple share

--- concurrency-go/multiple-share/share.go
+++ concurrency-go/multiple-share/share.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 const characters = "abcdefghijklmnopqrstuvwxyz"
 
-func CheckLetters(url string, freq []int) {
+func CheckLetters(url string, freq []int, mutex *sync.Mutex) {
 	resp, err := http.Get(url)
 	if err != nil {
 		panic("Error in Get")
@@ -20,6 +22,7 @@ func CheckLetters(url string, freq []int) {
 		panic("Error with status: " + resp.Status)
 	}
 	body, _ := io.ReadAll(resp.Body)
+	mutex.Lock()
 	for _, c := range body {
 		t1 := strings.ToLower(string(c))
 		t1Index := strings.Index(characters, t1)
@@ -27,18 +30,22 @@ func CheckLetters(url string, freq []int) {
 			freq[t1Index] += 1
 		}
 	}
+	mutex.Unlock()
 	fmt.Println(url, " completed")
 }
 
 func main() {
 	freq := make([]int, 26)
-	url := "https://example.com"
-	for i := 0; i < 10; i++ {
+	url := "https://jsonplaceholder.typicode.com/users/"
+	mutex := sync.Mutex{}
+	for i := 1; i < 11; i++ {
 		// this will may cause race conditions
-		go CheckLetters(url, freq)
+		go CheckLetters(url+strconv.Itoa(i), freq, &mutex)
 	}
 	time.Sleep(5 * time.Second)
+	mutex.Lock()
 	for t1, t2 := range characters {
 		fmt.Printf("%c is: %d\n", t2, freq[t1])
 	}
+	mutex.Unlock()
 }
