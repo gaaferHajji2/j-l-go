0f11076d48abeb06ba429fb4dad99ae51a39d30e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-03 10:15:39 +0300

    Adding Query data example

--- Rest-API-01/main.go
+++ Rest-API-01/main.go
@@ -47,6 +47,7 @@ func main() {
 	{
 		r2.GET("/image/:id", getImageById)
 		r2.GET("/:username", getProfileByUsername)
+		r2.GET("/", showData)
 	}
 
 	r.Run(":8001")
@@ -71,5 +72,15 @@ func getProfileByUsername(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"username": c.Param("username"),
 		"msg":      "Profile Ok",
+	})
+}
+
+func showData(c *gin.Context) {
+	// http://localhost:8001/profile/?id=2&jobs[0]=IT&jobs=developer&jobs=designer&jobs=manager
+	// &data[id]=100&data[name]=JLoka&data[type]=Employee
+	c.JSON(http.StatusOK, gin.H{
+		"id":   c.Query("id"),
+		"jobs": c.QueryArray("jobs"),
+		"data": c.QueryMap("data"),
 	})
 }
