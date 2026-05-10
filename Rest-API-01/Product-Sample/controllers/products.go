e41ff535debca6dbd504c2ba304419fb4e67553b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-11 02:38:42 +0300

    Build create product for controller

--- Rest-API-01/Product-Sample/controllers/products.go
+++ Rest-API-01/Product-Sample/controllers/products.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "jloka/jloka/models"
+import (
+	"jloka/jloka/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ProductRepo struct {
 	Products *[]models.Product
@@ -8,4 +13,23 @@ type ProductRepo struct {
 
 func Init(products *[]models.Product) *ProductRepo {
 	return &ProductRepo{Products: products}
+}
+
+// Create Product For Controller
+func (productRepo *ProductRepo) CreateProduct(c *gin.Context) {
+	var product *models.Product
+
+	if err := c.BindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Product Request: " + err.Error(),
+		})
+		return
+	}
+	if err := product.CreateProduct(productRepo.Products); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"msg": "ISE: " + err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusCreated, product)
 }
