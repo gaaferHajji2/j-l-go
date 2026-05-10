82439a3736341cc9805487253aab570ca048c65a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 03:16:27 +0300

    Test the caculator using packages & modules

---
+++ Rest-API-01/calculator/calc.go
@@ -0,0 +1,64 @@
+package calculator
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Numeric struct {
+	Num1 float64 `json:"num1" binding:"required"`
+	Num2 float64 `json:"num2" binding:"required"`
+}
+
+func Add(c *gin.Context) {
+	var body Numeric
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Request",
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result is: ": body.Num1 + body.Num2,
+	})
+}
+
+func Subtract(c *gin.Context) {
+	var body Numeric
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Request",
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result is: ": body.Num1 - body.Num2,
+	})
+}
+
+func Multiply(c *gin.Context) {
+	var body Numeric
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Request",
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result is: ": body.Num1 * body.Num2,
+	})
+}
+
+func Divide(c *gin.Context) {
+	var body Numeric
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Invalid Request",
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result is: ": body.Num1 / body.Num2,
+	})
+}
