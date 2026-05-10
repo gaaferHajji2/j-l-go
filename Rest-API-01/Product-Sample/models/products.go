be90465d0a964167a8851ebfe6314f65390e58e2
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-11 01:59:11 +0300

    Create the product model

--- Rest-API-01/Product-Sample/models/products.go
+++ Rest-API-01/Product-Sample/models/products.go
@@ -1,4 +1,8 @@
 package models
 
 type Product struct {
+	Id    int     `json:"id" binding:"required"`
+	Name  string  `json:"name" binding:"required"`
+	Stock int     `json:"stock" binding:"required"`
+	Price float32 `json:"price" binding:"required"`
 }
