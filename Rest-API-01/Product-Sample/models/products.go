4925e03a3945b6218d06caefa2c46b570f2f82db
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-11 02:03:10 +0300

    Build the create product method

--- Rest-API-01/Product-Sample/models/products.go
+++ Rest-API-01/Product-Sample/models/products.go
@@ -6,3 +6,10 @@ type Product struct {
 	Stock int     `json:"stock" binding:"required"`
 	Price float32 `json:"price" binding:"required"`
 }
+
+// Create Product
+func CreateProduct(products *[]Product, product *Product) (result *[]Product, err error) {
+	*products = append(*products, *product)
+
+	return products, nil
+}
