db903f370e2170cc25ec3821a7ab712cd2caf7da
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-17 03:42:33 +0300

    Build product rest method for testing

--- Rest-API-01/Product-Sample/controllers/products.go
+++ Rest-API-01/Product-Sample/controllers/products.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"jloka/jloka/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -32,4 +34,65 @@ func (productRepo *ProductRepo) CreateProduct(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, product)
+}
+
+func (productRepo *ProductRepo) DeleteProduct(c *gin.Context) {
+	Id, _ := strconv.Atoi(c.Param("id"))
+	for _, product := range *productRepo.Products {
+		if product.Id == Id {
+			product.DeleteProductById(productRepo.Products)
+			c.JSON(http.StatusNoContent, gin.H{
+				"msg": "Data Deleted Successfully",
+			})
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{
+		"msg": "No Data Found",
+	})
+}
+
+func (productRepo *ProductRepo) GetProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, productRepo.Products)
+}
+
+func (productRepo *ProductRepo) GetProductById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	product := models.GetProductById(productRepo.Products, id)
+	if product.Name != "" {
+		fmt.Println("The Product Name is: ", product.Name)
+		c.JSON(http.StatusNotFound, gin.H{})
+	}
+	c.JSON(http.StatusOK, product)
+}
+
+func (productRepo *ProductRepo) UpdateProductById(c *gin.Context) {
+	id, err := strconv.Atoi("id")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	tempProduct := models.GetProductById(productRepo.Products, id)
+
+	if tempProduct.Id == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg": "Product Not Found For Updating",
+		})
+		return
+	}
+
+	product := models.UpdateProductById(productRepo.Products, tempProduct)
+
+	if product.Name != "" {
+		fmt.Println("The Product Name is: ", product.Name)
+		c.JSON(http.StatusNotFound, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
 }
