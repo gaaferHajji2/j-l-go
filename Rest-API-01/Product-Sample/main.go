b10047a1ee3731b348e0ae2cef3ddd4530c38a17
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-17 03:43:02 +0300

    Create the router handler for the rest api test

--- Rest-API-01/Product-Sample/main.go
+++ Rest-API-01/Product-Sample/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"jloka/jloka/controllers"
+	"jloka/jloka/models"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	var products []models.Product
+	var productRepo controllers.ProductRepo = *controllers.Init(&products)
+
+	r.POST("/products", productRepo.CreateProduct)
+	r.GET("/products", productRepo.GetProducts)
+	r.GET("/products/:id", productRepo.GetProductById)
+	r.PUT("/products/:id", productRepo.UpdateProductById)
+	r.DELETE("/products/:id", productRepo.DeleteProduct)
+
+	r.Run()
 }
